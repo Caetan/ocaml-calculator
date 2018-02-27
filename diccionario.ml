b4262319b0196656aedda70c351b0bd7450dfd5a
Caetan <caetantojeiro95@gmail.com>
2018-02-27 20:20:01 +0100

    licencia

--- diccionario.ml
+++ diccionario.ml
@@ -4,6 +4,24 @@
 
 
 	Developed by Caetán Tojeiro Carpente (caetantojeiro95@gmail.com)
+	
+	
+
+Copyright (C) 2017  Caetán Tojeiro Carpente
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/agpl-3.0.html>
+
 
 
 ************************************************************************)
